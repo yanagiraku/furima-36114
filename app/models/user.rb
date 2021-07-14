783128f48a4ba5496fd01a4c8b0cbb3ad1764ebb
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-07-14 18:54:50 +0900

    モデルのバリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :email,           uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :encrypted_password, length: { minimum: 6 }, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday,        presence: true
+
+
+end
