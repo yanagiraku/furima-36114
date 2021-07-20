1691d9abb589971eab9c89e449adfd70062c236f
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-07-20 15:37:38 +0900

    販売手数料・販売利益の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,29 @@
 class Item < ApplicationRecord
-  # belongs_to :user
-  # has_one :purchase
+  belongs_to :user
+  has_one :purchase
+
+  validates :item_name,        length:{ maximum: 40 }
+  validates :price,            numericality: { only_integer: true, in: 300..9999999 }
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 }  do
+    validates :category_id
+    validates :status_id
+    validates :burden_id
+    validates :delivery_id
+    validates :day_delivery_id
+  end
 
 
-  # validates :item_name,        presence: true
-  # validates :category_id,      presence: true
-  # validates :item_description, presence: true
-  # validates :status_id,        presence: true
-  # validates :burden_id,        presence: true
-  # validates :delivery_id,      presence: true
-  # validates :day_delivery_id,  presence: true
-  # validates :price,            presence: true
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :delivery
+  belongs_to :day_delivery
 end
