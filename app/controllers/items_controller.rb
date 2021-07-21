21288d9352f9e6519c6e375cfcde31605b0f8666
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-07-21 19:07:34 +0900

    商品情報編集機能　訂正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
-  
+
   def index
     @items = Item.all.order(created_at: :desc)
 
@@ -43,7 +43,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to action: :index
     end
