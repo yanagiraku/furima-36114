a92251b9bf43d0572fdd127b396ad27650c83d42
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-07-21 18:26:46 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
   def index
     @items = Item.all.order(created_at: :desc)
 
@@ -22,8 +23,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
   def item_params
     params.require(:item).permit(:image, :item_name, :category_id, :item_description, :status_id, :burden_id, :delivery_id, :day_delivery_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
 
