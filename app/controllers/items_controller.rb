05f39dc48cdce0db83ab10bb122f8d9870b39087
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-07-21 18:56:13 +0900

    商品情報編集機能　訂正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
+  
   def index
     @items = Item.all.order(created_at: :desc)
 
