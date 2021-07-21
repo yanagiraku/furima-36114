47a02ce2ee61563762f5570b12c6fd65e0337ce3
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-07-21 09:57:56 +0900

    テストコード訂正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,15 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  def index
-    @items = Item.order("created_at DESC")
-
-  end
+  # def index
+  #   @items = Item.order("created_at DESC")
+  # end
 
   def new
     @item = Item.new
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -89,14 +89,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
 
+      it "priceが半角英数字混合では登録できない" do
+        @item.price = "100aa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it "priceが半角英字のみでは登録できない" do
+        @item.price = "aaaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+
       it "priceが300より安価であれば登録できない" do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
       it "priceが9999999より高価であれば登録できない" do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
