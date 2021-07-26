7f887de351642e45ee5065dc281feefc8d758e7f
yanagiraku <n.miu.ovo.november19@gmail.com>
2021-07-26 15:41:15 +0900

    購入機能　訂正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
-  before_action :sold_out_item, only: [:index]
+  before_action :sold_out_item, only: [:index, :create]
 
   def index
     if current_user.id == @item.user_id
